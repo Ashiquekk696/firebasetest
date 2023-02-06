07ba923121827a6717caebdbe325121a3dd0eb90
Ashique KK <ashique@designfort.com>
2023-02-06 14:30:04 +0530

    Initial commit

---
+++ lib/services/auth_service.dart
@@ -0,0 +1,14 @@
+import 'dart:js';
+
+import 'package:flutter/cupertino.dart';
+
+class AuthService{
+  handleAuthState(){
+    return StreamBuilder(builder: (context,snapsho){
+
+      if(snapsho.hasData){
+        return HomeScre
+      }
+    })
+  }
+}
